ce42a7fd6e7e9f319d509101fe499b1be0709e8a
adithya64 <adb6464-2@yahoo.com>
2018-07-11 16:53:51 -0700

    0.0.1 First test

---
+++ Work Area/AFSDKTest/AFSDKTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OSIsoft.AF;
+using OSIsoft.AF.Asset;
+using OSIsoft.AF.UnitsOfMeasure;
+
+namespace AFSDKTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AFDatabase database = GetDatabase("PU-PIAFAPPDEV", "Green Power Company");
+
+
+        }
+
+        static AFDatabase GetDatabase(string server, string database)
+        {
+            PISystems piSystems = new PISystems();
+            PISystem assetServer = piSystems[server];
+            AFDatabase afDatabase = assetServer.Databases[database];
+            return afDatabase;
+        }
+
+        static void PrintRootElements(AFDatabase database)
+        {
+            Console.WriteLine("Print Root Elements: {0}", database.Elements.Count);
+            foreach (AFElement element in database.Elements)
+            {
+                Console.WriteLine("  {0}", element.Name);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
